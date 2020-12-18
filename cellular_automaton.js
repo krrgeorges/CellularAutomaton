3265bcd8868e94416b4ab29e3f63c876afea02bb
rrgeorge-pdcontributions <dracovault@gmail.com>
2020-12-18 16:58:05 +0530

    grid toggle added

--- cellular_automaton.js
+++ cellular_automaton.js
@@ -1,3 +1,6 @@
+
+
+
 var drawMode = false;;var bgcolor = "red";var pencilColor = "blue";var n_generation = 0;var epoch_mech = null;var h = null;var w = null;
 var tds = {};var element = null;var td = null;var populated = false;var neighbors = 0;var mtds=[];
 var alive = 0
@@ -13,7 +16,8 @@ function buildGrid(m,n,bgcolor,pencilColor,randomized){
 	    tr = document.createElement("tr")
 	    for(var j=0;j<=n-1;j++){
 	        td = document.createElement("td")
-	        td.style.borderLeft = "0.01em solid black";td.style.borderTop = "0.01em solid black";td.style.width="5px";td.style.height="5px";
+	        // td.style.borderLeft = "0.01em solid black";td.style.borderTop = "0.01em solid black";
+	        td.style.width="5px";td.style.height="5px";
 	        if(randomized == 1){td.style.backgroundColor = Math.floor(Math.random() * 2)==0?bgcolor:pencilColor;}
 	        else{td.style.backgroundColor = bgcolor}
 	        td.dataset.i = i;td.dataset.j = j
@@ -35,7 +39,7 @@ function buildGrid(m,n,bgcolor,pencilColor,randomized){
 function obtainRelativeTD(xi,yj){
 	if(tds[xi.toString()+"_"+yj.toString()]==undefined){
 		xi = (xi<0)?h-1:(xi>h-1)?0:xi
-		yj = (yj<0)?w-1:(yj>h-1)?0:yj
+		yj = (yj<0)?w-1:(yj>w-1)?0:yj
 	}
 
 	element = tds[xi.toString()+"_"+yj.toString()]
@@ -61,13 +65,9 @@ function simulate(){
 		neighbors = 0;
 		pos = [[parseInt(td.dataset.i)-1,parseInt(td.dataset.j)],[parseInt(td.dataset.i)-1,parseInt(td.dataset.j)-1],[parseInt(td.dataset.i)-1,parseInt(td.dataset.j)+1],[parseInt(td.dataset.i)+1,parseInt(td.dataset.j)],[parseInt(td.dataset.i)+1,parseInt(td.dataset.j)-1],[parseInt(td.dataset.i)+1,parseInt(td.dataset.j)+1],[parseInt(td.dataset.i),parseInt(td.dataset.j)-1],[parseInt(td.dataset.i),parseInt(td.dataset.j)+1]]
 		for(p in pos){
-			if(obtainRelativeTD(pos[p][0],pos[p][1]) == "blue"){
-				if(document.getElementById("nb_type").value == 0){
-					neighbors = neighbors+1
-				}
-				else{
-					neighbors += neighbors+1
-				}
+			if(obtainRelativeTD(pos[p][0],pos[p][1]) == pencilColor){
+				if(document.getElementById("nb_type").value == 0){neighbors = neighbors+1}
+				else{neighbors += neighbors+1}
 			}
 		}
 		if(populated == true){
@@ -99,14 +99,61 @@ function stopGOL(){
 
 
 
+function drawLexicon(lexicon){
+	var lexi_base = [
+					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"█",0,0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"█",0,"█",0,0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0,0,0,"█","█",0,0,0,0,0,0,"█","█",0,0,0,0,0,0,0,0,0,0,0,0,"█","█"],
+					 [0,0,0,0,0,0,0,0,0,0,0,"█",0,0,0,"█",0,0,0,0,"█","█",0,0,0,0,0,0,0,0,0,0,0,0,"█","█"],
+					 ["█","█",0,0,0,0,0,0,0,0,"█",0,0,0,0,0,"█",0,0,0,"█","█",0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					 ["█","█",0,0,0,0,0,0,0,0,"█",0,0,0,"█",0,"█","█",0,0,0,0,"█",0,"█",0,0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0,"█",0,0,0,0,0,"█",0,0,0,0,0,0,0,"█",0,0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0,0,"█",0,0,0,"█",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0,0,0,0,"█","█",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+	var si = 15;var sj=15;
+	for(var i=0;i<=lexi_base.length-1;i++){
+		var row = lexi_base[i]
+		for(var j=0;j<=row.length-1;j++){
+			if(row[j]!=0){
+				document.getElementById( (si+i).toString()+"_"+(sj+j).toString() ).style.backgroundColor = pencilColor
+			}
+		}
+	}
+}
+
+
+
 
 window.onload = function(){
 	h = Math.round(window.innerHeight/7)
 	w = Math.round(window.innerWidth/5)
 	buildGrid(h,w,bgcolor,pencilColor,0)
+	document.getElementById("gridStatus").onclick = function(){
+		if(document.getElementById("gridStatus").dataset.status == "0"){
+			document.getElementById("gridStatus").dataset.status = "1"
+			var tds = document.getElementById("mainGrid").getElementsByTagName("td");
+			for(td in tds){
+				try{tds[td].style.borderLeft = "1px solid black"}
+				catch(err){continue}
+				tds[td].style.borderTop = "1px solid black"
+			}
+			document.getElementById("gridStatus").innerText = "GRID : ON"
+		}
+		else{
+			document.getElementById("gridStatus").dataset.status = "0"
+			var tds = document.getElementById("mainGrid").getElementsByTagName("td");
+			for(td in tds){
+				try{tds[td].style.borderLeft = ""}
+				catch(err){continue}
+				tds[td].style.borderTop = ""
+			}
+			document.getElementById("gridStatus").innerText = "GRID : OFF"
+		}
+	}
 	document.getElementById("bgColor").onchange=function(){bgcolor=document.getElementById("bgColor").value;buildGrid(h,w,bgcolor,pencilColor)}
 	document.getElementById("pencilColor").onchange=function(){pencilColor=document.getElementById("pencilColor").value;buildGrid(h,w,bgcolor,pencilColor)}
 	document.getElementById("pattern_type").onchange=function(){buildGrid(h,w,bgcolor,pencilColor,document.getElementById("pattern_type").value)}
 	document.getElementById("start").onclick = startGOL
 	document.getElementById("stop").onclick = stopGOL
+	// drawLexicon()
 }
